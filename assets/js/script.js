ef9fa93d2caa8c9b3595f38acc4035a31eaaedd9
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2024-11-22 20:18:07 +0000

    Insert code for input values and validation inside calculate button eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,34 @@
-// Get necessary input values
-let age = document.getElementById("age").value;
-let height = document.getElementById("height").value;
-let weight = document.getElementById("weight").value;
+// Retrieve calculation button and add function to retrieve input values and then, form validation
+let calculateButton = document.getElementById("calculate-button");
+calculateButton.addEventListener('click', function () {
 
-let bodyType;
-if (document.getElementById("male").checked) {
-    bodyType = 'male';
-} else (document.getElementById("female").checked) {
-    bodyType = "female";
-}
+    // Get necessary input values
+    let age = document.getElementById("age").value;
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
 
-// Making sure to validate the form
-if (!age) {
-    alert(`Please add your age`);
-} else if (!height) {
-    alert(`Please add your height`);
-} else if (!weight) {
-    alert(`Please add your weight`);
-} else if (!bodyType) {
-    alert(`Please select your body-type`);
-}
+    let bodyType;
+    if (document.getElementById('male').checked) {
+        bodyType = 'male';
+    } else if (document.getElementById('female').checked) {
+        bodyType = 'female';
+    }
 
+    // Making sure to validate the form
+    if (!age) {
+        alert("Please add your age");
+    } else if (!height) {
+        alert("Please add your height");
+    } else if (!weight) {
+        alert("Please add your weight");
+    } else if (!bodyType) {
+        alert("Please select your body-type");
+    }
+    console.log({age, height, weight, bodyType})
+});
 
-function bmiCalculation();
+function bmiCalculation() {
 
-function bmrCalculation();
+};
+
+// function bmrCalculation();
