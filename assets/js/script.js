7681feb591b6e912f1fd6511a94ea63b5de975fc
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2024-11-22 19:20:30 +0000

    Add code to get form input values, and form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+// Get necessary input values
+let age = document.getElementById("age").value;
+let height = document.getElementById("height").value;
+let weight = document.getElementById("weight").value;
+
+let bodyType;
+if (document.getElementById("male").checked) {
+    bodyType = 'male';
+} else (document.getElementById("female").checked) {
+    bodyType = "female";
+}
+
+// Making sure to validate the form
+if (!age) {
+    alert(`Please add your age`);
+} else if (!height) {
+    alert(`Please add your height`);
+} else if (!weight) {
+    alert(`Please add your weight`);
+} else if (!bodyType) {
+    alert(`Please select your body-type`);
+}
+
+
+function bmiCalculation();
+
+function bmrCalculation();
