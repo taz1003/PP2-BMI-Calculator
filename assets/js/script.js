952cc6e80a43adf04237aea8764da0e77cd4b1c9
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2024-11-23 19:01:00 +0000

    Add code to calculate BMI

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@ calculateButton.addEventListener('click', function () {
 
     // Get necessary input values
     let age = parseInt(document.getElementById("age").value);
-    let height = parseFloat(document.getElementById("height").value);
+    // Convert height value from m to cm
+    let height = parseFloat(document.getElementById("height").value) / 100;
     let weight = parseFloat(document.getElementById("weight").value);
 
     let bodyType;
@@ -24,11 +25,17 @@ calculateButton.addEventListener('click', function () {
     } else if (!bodyType) {
         alert("Please select your body-type");
     }
-    console.log({age, height, weight, bodyType})
+
+    // Call the BMI calculation function
+    bmiCalculation(weight, height);
 });
 
-function bmiCalculation() {
-
+/**
+ * Calculates value of BMI using values of height and weight
+ */
+function bmiCalculation(weight, height) {
+    let userBmi = weight / (height * height);
+    let roundedUserBmi = Math.round(userBmi * 100) / 100;
+    document.getElementById("bmi-value").textContent = `${roundedUserBmi}`
 }
-
 // function bmrCalculation();
